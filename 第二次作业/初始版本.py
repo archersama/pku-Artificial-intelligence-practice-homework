22758fb71b7c61e4c025221446012dea5a367ec9
archersama <946971688@qq.com>
2019-10-14 19:02:17 +0800

    Update 初始版本.py

--- 第二次作业/初始版本.py
+++ 第二次作业/初始版本.py
@@ -1,26 +1,38 @@
 # !/usr/bin/env python
 # coding:utf-8
 # Author: Lixiangyang
-
-#网络模型采用最简单的bp神经网络，一共三层，输入层，隐藏层，输出层，最后的准确率为80%左右
-
+#将所有数据集用于训练,并且使用了relu函数，得到的准确率为100%
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
-from sklearn import datasets#从sklearn中得到鸢尾花的数据集
-x_data = datasets.load_iris().data#x_data为鸢尾花的花萼长度，宽度，花瓣长度，宽度，共组
-y_data = datasets.load_iris().target#y——data为鸢尾花的标签
+#from sklearn import datasets#从sklearn中得到鸢尾花的数据集
+import pandas as pd
+iris_data=pd.read_csv('iris.txt',header=None)#读取数据
+iris_data.replace("Iris-setosa",0,inplace = True)#相应的数据转为数字对应的类别
+iris_data.replace("Iris-versicolor",1,inplace = True)
+iris_data.replace("Iris-virginica",2,inplace = True)
+data=np.array(iris_data)#转化为array类方便处理
+x_data=np.array(data[:,[0,1,2,3]])
+y_data=np.array(data[:,[-1]])
+y_data=y_data.reshape(150)
+y_data=y_data.astype(int)
+#从自带的数据集中读取数据
+#x_data = datasets.load_iris().data#x_data为鸢尾花的花萼长度，宽度，花瓣长度，宽度，共150组
+#y_data = datasets.load_iris().target#y——data为鸢尾花的标签
+
+#x_data = datasets.load_iris().data
 np.random.seed(116)#使用seed对数据集进行扰乱
 np.random.shuffle(x_data)
 np.random.seed(116)
 np.random.shuffle(y_data)
-x_train=x_data[:-30]#取出前120个数据集作为训练集
-y_train=y_data[:-30]
+x_train=x_data#取出前150个数据集作为训练集
+y_train=y_data
 x_test=x_data[-30:]#取出后三十个作为测试集
 y_test=y_data[-30:]
 
 x_train = tf.cast(x_train, tf.float32)#在进行矩阵相乘时需要float型，故强制类型转换为float型
+print(x_train.shape)
 x_test=tf.cast(x_test,tf.float32)
 
 # from_tensor_slices函数切分传入的 Tensor 的第一个维度，生成相应的 dataset
@@ -34,6 +46,8 @@ w1 = tf.Variable(tf.random.truncated_normal([4, 32], stddev=0.1, seed=1))#w1为4
 b1 = tf.Variable(tf.random.truncated_normal([32], stddev=0.1, seed=1))#b1位32行，1列的参数，正态分布
 w2 = tf.Variable(tf.random.truncated_normal([32, 32], stddev=0.1,seed=2))#w2为第二层参数
 b2=tf.Variable(tf.random.truncated_normal([32], stddev=0.1,seed=2))
+w2 = tf.Variable(tf.random.truncated_normal([32, 32], stddev=0.1,seed=2))#w2为第二层参数
+b2=tf.Variable(tf.random.truncated_normal([32], stddev=0.1,seed=2))
 w3 = tf.Variable(tf.random.truncated_normal([32, 3], stddev=0.1,seed=3))#w3为第三层参数
 b3=tf.Variable(tf.random.truncated_normal([3], stddev=0.1,seed=3))
 lr = 0.1#学习率
@@ -44,7 +58,9 @@ for epoch in range(epoch):  #数据集级别迭代
     for step,(x_train,y_train) in enumerate(train_db):  #batch级别迭代
         with tf.GradientTape() as tape:
             h1 = tf.matmul(x_train, w1) + b1
-            h2 = tf.matmul(h1, w2) + b2
+            h11=tf.nn.relu(h1)
+            h2 = tf.matmul(h11, w2) + b2
+            h22 = tf.nn.relu(h2)
             y = tf.matmul(h2,w3) + b3
             y_onehot = tf.one_hot(y_train, depth=3)#采用独热编码
 
@@ -70,8 +86,10 @@ for epoch in range(epoch):  #数据集级别迭代
     total_correct, total_number = 0, 0
     for step, (x_train, y_train) in enumerate(test_db):
         h1 = tf.matmul(x_train, w1) + b1
-        h2 = tf.matmul(h1, w2) + b2
-        y = tf.matmul(h2, w3) + b3
+        h11 = tf.nn.relu(h1)
+        h2 = tf.matmul(h11, w2) + b2
+        h22 = tf.nn.relu(h2)
+        y = tf.matmul(h22, w3) + b3
 
         pred = tf.argmax(y, axis=1)#返回y每一行的最大值
 
